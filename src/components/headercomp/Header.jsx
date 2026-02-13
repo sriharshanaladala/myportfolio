e69ee98b07112edf71fb850a766c4812a22128fc
sriharshanaladala <83389426+sriharshanaladala@users.noreply.github.com>
2026-02-13 17:42:51 +0000

    Fix: remove unused vars from Header to satisfy ESLint/CI

--- src/components/headercomp/Header.jsx
+++ src/components/headercomp/Header.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import '../../stylings/darklightmode.css';
 
-const githubUrl = process.env.REACT_APP_GITHUB_URL;
-const linkedinUrl = process.env.REACT_APP_LINKEDIN_URL;
-const mailTo = `mailto:${process.env.REACT_APP_EMAIL}`;
-
-const Header = ({ darkMode, setDarkMode }) => {
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-    };
-
-    return (
-        <header className="header" />
-    );
+const Header = () => {
+    return <header className="header" />;
 };
 
 export default Header;
